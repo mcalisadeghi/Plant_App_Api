7c9d4c593e069057f6151d3b9a7b1a3253186e0b
mcalisadeghi <ali.ke970@gmail.com>
2023-12-04 18:34:37 +0330

    build root

---
+++ lib/ui/root/root.dart
@@ -0,0 +1,115 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/constants/constants.dart';
+import 'package:page_transition/page_transition.dart';
+
+class RootPage extends StatefulWidget {
+  const RootPage({super.key});
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  int bottomIndex = 0;
+
+  List<Plant> favorites = [];
+  List<Plant> myCart = [];
+
+  List<Widget> page() {
+    return [
+      const HomePage(),
+      FavoritePage(favoritedPlants: favorites),
+      CartPage(addedToCartPlants: myCart),
+      const ProfilePage(),
+    ];
+  }
+
+  List<IconData> iconList = [
+    Icons.home,
+    Icons.favorite,
+    Icons.shopping_cart,
+    Icons.person,
+  ];
+
+  List<String> appBarTitle = [
+    'خانه',
+    'علاقه‌مندی‌ها',
+    'سبد‌خرید',
+    'پروفایل',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Padding(
+          padding: const EdgeInsets.only(top: 30.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Icon(
+                color: Constants.blackColor,
+                Icons.notifications,
+                size: 30.0,
+              ),
+              Text(
+                appBarTitle[bottomIndex],
+                style: TextStyle(
+                  color: Constants.blackColor,
+                  fontFamily: 'Lalezar',
+                  fontSize: 24.0,
+                ),
+              ),
+            ],
+          ),
+        ),
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        elevation: 0.0,
+      ),
+      body: IndexedStack(
+        index: bottomIndex,
+        children: page(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            PageTransition(
+              child: const ScanPage(),
+              type: PageTransitionType.bottomToTop,
+            ),
+          );
+        },
+        backgroundColor: Constants.primaryColor,
+        child: Image.asset(
+          'assets/images/code-scan-two.png',
+          height: 30.0,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: AnimatedBottomNavigationBar(
+        splashColor: Constants.primaryColor,
+        activeColor: Constants.primaryColor,
+        activeIndex: bottomIndex,
+        inactiveColor: Colors.black.withOpacity(0.5),
+        gapLocation: GapLocation.center,
+        icons: iconList,
+        notchSmoothness: NotchSmoothness.softEdge,
+        onTap: (index) {
+          setState(
+            () {
+              bottomIndex = index;
+
+              final List<Plant> favoritedPlants = Plant.getFavoritedPlants();
+              final List<Plant> addedToCartPlants = Plant.addedToCartPlants();
+
+              favorites = favoritedPlants.toSet().toList();
+              myCart = addedToCartPlants.toSet().toList();
+            },
+          );
+        },
+      ),
+    );
+  }
+}
